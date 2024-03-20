c3211ad2320c49fa196c8e4a250871f0aa2f7e31
Sandeep Kumar <sandepten@gmail.com>
2024-03-20 15:37:36 +0530

    Remove dotenv environment variable files and add build directory to .gitignore

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,43 +1,41 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import "./Login.css";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import { baseurl } from "../../api/apiConfig";
-import { toast } from "react-toastify";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { baseurl } from '../../api/apiConfig';
+import './Login.css';
 
 function Login() {
   let navigate = useNavigate();
-  let navigate = useNavigate();
 
   const [loginData, setLoginData] = useState({
-    username: "",
-    password: "",
+    username: '',
+    password: '',
   });
 
   const submitLogin = () => {
     const data = loginData;
     console.log(data);
     axios({
-      method: "post",
+      method: 'post',
       url: `${import.meta.env.VITE_API_URL}/mhere/login`,
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       data,
     })
       .then(function (response) {
         console.log(response);
-        localStorage.setItem("employee_id", response.data.new_e_code);
-        localStorage.setItem("manager_id", response.data.reporting_manager_id);
-        localStorage.setItem("token", response.data.token);
-        localStorage.setItem("email", response.data.email);
-        localStorage.setItem("grade", response.data.grade);
-        localStorage.setItem("band", response.data.band);
-        localStorage.setItem("role", response.data.role);
-        localStorage.setItem("department", response.data.department);
-        localStorage.setItem("fullname", response.data.name_of_the_eployee);
+        localStorage.setItem('employee_id', response.data.new_e_code);
+        localStorage.setItem('manager_id', response.data.reporting_manager_id);
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('email', response.data.email);
+        localStorage.setItem('grade', response.data.grade);
+        localStorage.setItem('band', response.data.band);
+        localStorage.setItem('role', response.data.role);
+        localStorage.setItem('department', response.data.department);
+        localStorage.setItem('fullname', response.data.name_of_the_eployee);
 
         /*   const data = {
         /*   const data = {
@@ -56,11 +54,11 @@ function Login() {
           employee_id: response.data.new_e_code,
         };
         axios({
-          method: "post",
+          method: 'post',
           url: `${baseurl.base_url}/mhere/get-employee-module-access`,
           headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
           },
           data,
         })
@@ -69,102 +67,95 @@ function Login() {
 
             if (res.data.data?.ceam_management) {
               if (response.data.is_first_login) {
-                setLoginData({ ...loginData, username: "", password: "" });
-                window.location.href =
-                  "https://internal.microtek.tech:8443/ess/forget-password";
+                setLoginData({ ...loginData, username: '', password: '' });
+                window.location.href = 'https://internal.microtek.tech:8443/ess/forget-password';
 
                 return;
               }
-              localStorage.setItem(
-                "module_access",
-                JSON.stringify(res.data.data)
-              );
+              localStorage.setItem('module_access', JSON.stringify(res.data.data));
 
               let data = {
                 employee_id: response.data.new_e_code,
-                module_name: "Ceam",
+                module_name: 'Ceam',
               };
               axios({
-                method: "post",
+                method: 'post',
                 url: `${baseurl.base_url}/mhere/log-login`,
                 headers: {
-                  "Content-Type": "application/json",
-                  Authorization: `Bearer ${localStorage.getItem("token")}`,
+                  'Content-Type': 'application/json',
+                  Authorization: `Bearer ${localStorage.getItem('token')}`,
                 },
                 data,
               });
-              navigate("/home");
+              navigate('/home');
               window.location.reload();
             } else {
-              alert("module access not allowed");
-              document.getElementById("login-button").disabled = false;
+              alert('module access not allowed');
+              document.getElementById('login-button').disabled = false;
             }
             //localStorage.setItem('module_access', JSON.stringify(res.data.data))
           })
           .catch((err) => {
             toast.error(err.response.data.message);
-            document.getElementById("login-button").disabled = false;
+            document.getElementById('login-button').disabled = false;
             console.log(err);
           });
       })
       .catch(function (err) {
         toast.error(err.response.data.message);
         console.log(err);
-        document.getElementById("login-button").disabled = false;
+        document.getElementById('login-button').disabled = false;
       });
   };
 
   return (
-    <div className="logi-main">
-      <div className="logi-main-left">
+    <div className='logi-main'>
+      <div className='logi-main-left'>
         <h3>CEAM</h3>
-        <div className="logi-main-left-container">
-          <form className="logi-main-left-inner">
-            <div className="logi-main-left-header">
+        <div className='logi-main-left-container'>
+          <form className='logi-main-left-inner'>
+            <div className='logi-main-left-header'>
               <h3>Welcome Back TEAM !</h3>
 
               <p> Please enter your details</p>
             </div>
-            <div className="logi-input-main">
-              <div className="logi-input-inner">
-                <label htmlFor="emp">Employee Id</label>
+            <div className='logi-input-main'>
+              <div className='logi-input-inner'>
+                <label htmlFor='emp'>Employee Id</label>
                 <input
-                  id="emp"
-                  placeholder="Enter Your Employee Code"
+                  id='emp'
+                  placeholder='Enter Your Employee Code'
                   value={loginData.username}
                   onChange={(e) => {
                     setLoginData({ ...loginData, username: e.target.value });
                   }}
-                  type="text"
+                  type='text'
                 />
               </div>
-              <div className="logi-input-inner">
-                <label htmlFor="password">Password</label>
+              <div className='logi-input-inner'>
+                <label htmlFor='password'>Password</label>
                 <input
-                  id="password"
-                  type="password"
-                  placeholder="Enter Password"
+                  id='password'
+                  type='password'
+                  placeholder='Enter Password'
                   onChange={(e) => {
                     setLoginData({ ...loginData, password: e.target.value });
                   }}
                 />
               </div>
-              <div className="logi-forget-link">
-                <a
-                  href="https://internal.microtek.tech:8443/ess/forget-password"
-                  target="_blank"
-                >
+              <div className='logi-forget-link'>
+                <a href='https://internal.microtek.tech:8443/ess/forget-password' target='_blank'>
                   Change / Forgot Password?
                 </a>
               </div>
             </div>
-            <div className="logi-buttons-main">
+            <div className='logi-buttons-main'>
               <button
-                type="submit"
-                id="login-button"
+                type='submit'
+                id='login-button'
                 onClick={(e) => {
-                  console.log("submit");
-                  document.getElementById("login-button").disabled = true;
+                  console.log('submit');
+                  document.getElementById('login-button').disabled = true;
                   e.preventDefault();
                   submitLogin();
                 }}
@@ -175,10 +166,8 @@ function Login() {
           </form>
         </div>
       </div>
-      <div className="logi-main-right">
-        <div className="logi-main-right-inner">
-          {/*  <img src={microtek2} alt="" /> */}
-        </div>
+      <div className='logi-main-right'>
+        <div className='logi-main-right-inner'>{/*  <img src={microtek2} alt="" /> */}</div>
       </div>
     </div>
   );
